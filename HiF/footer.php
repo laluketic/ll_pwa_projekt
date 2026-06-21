e18469fa2695c457fd60f1433d311ebbc19fb3f3
laluketic <lluketic2019@gmail.com>
2026-06-21 20:58:28 +0200

    Add files via upload

---
+++ HiF/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="wrapper">
+        <p>Lana Luketić, lluketic1@tvz.hr, 2026.</p>
+    </div>
+</footer>
---
+++ HiF/header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="wrapper">
+        <div class="Dheader">
+            <div class="logo">
+                <img src="images/logo.png" alt="logo">
+            </div>
+            <nav class="nav">
+                <ul>
+                    <li><a href="index.php">POČETNA</a></li>
+                    <li><a href="kategorija.php?kategorija=aktualno">AKTUALNO</a></li>
+                    <li><a href="kategorija.php?kategorija=sport">SPORT</a></li>
+                    <li><a href="unos.php">UNOS</a></li>
+                    <li><a href="administracija.php">ADMINISTRACIJA</a></li>
+                    <li><a href="registracija.php">REGISTRACIJA</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
